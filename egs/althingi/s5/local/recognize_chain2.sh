11f82fdefa4802fbede0e2eb4b600fed3cddeed4
Inga Run <ingarunh@gmail.com>
2017-05-22 14:32:07 +0000

    Unique identities of denormalized transcripts

--- egs/althingi/s5/local/recognize_chain2.sh
+++ egs/althingi/s5/local/recognize_chain2.sh
@@ -120,7 +120,7 @@ if [ $stage -le 8 ]; then
     echo "Denormalize the transcript"
     local/denormalize.sh \
         ${rescoredir}/transcript_noID.txt \
-        ${rescoredir}/transcript_denormalized.txt
+        ${rescoredir}/${speechname}_denorm_transcript.txt
     rm ${rescoredir}/*.tmp
 fi
 

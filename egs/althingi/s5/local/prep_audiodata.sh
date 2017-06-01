42203f9f81d93475085aad4438f54d1c1662ed9c
Inga Run <ingarunh@gmail.com>
2017-06-01 09:00:43 +0000

    renamed and indep of audio extension

--- egs/althingi/s5/local/prep_audiodata.sh
+++ egs/althingi/s5/local/prep_audiodata.sh
@@ -2,77 +2,60 @@
 
 # Prepare a directory on Kaldi format, containing audio data and some auxiliary info.
 
-datadir=$1
-#destdir=$2
-#mkdir -p ${destdir}
-
 stage=-1
-nj=1
 
 . ./cmd.sh
 . ./path.sh
 . utils/parse_options.sh
 . local/utils.sh
 
-if ! [[ $n_files > 0 ]]; then
-    error "$audio_zip didn't contain audio files"
+speechname=$1
+ext=$2
+meta=$3
+datadir=$4
+
+encoding=$(file -i ${meta} | cut -d" " -f3)
+if [[ "$encoding" == "charset=iso-8859-1" ]]; then
+    iconv -f ISO-8859-1 -t UTF-8 ${meta} > tmp && mv tmp ${meta}
 fi
-
-encoding=$(file -i ${datadir}/metadata.csv | cut -d" " -f3)
-if [[ "$encoding"=="charset=iso-8859-1" ]]; then
-    iconv -f ISO-8859-1 -t UTF-8 ${datadir}/metadata.csv > tmp && mv tmp ${datadir}/metadata.csv
-fi
-
-# NOTE! Maybe I will be able to get name, ID and gender info from metadata. Check!
-# Make name-id-gender file from metadata info.
-
-name_id_file=${datadir}/name_id_gender.tsv
-meta=${datadir}/metadata.csv
 
 # NOTE! Could I get the audio files in wav format??
 # Need to convert from mp3 to wav
 samplerate=16000
 # SoX converts all audio files to an internal uncompressed format before performing any audio processing
-#wav_cmd="sox -tmp3 - -c1 -esigned -r$samplerate -G -twav - "
-wav_cmd="sox -tflac - -c1 -esigned -twav - "
+wav_cmd="sox -t$ext - -c1 -esigned -r$samplerate -G -twav - "
+#wav_cmd="sox -tflac - -c1 -esigned -twav - "
+
+IFS=$'\n' # Split on new line
 
 if [ $stage -le 0 ]; then
+
+    # Extract the speaker info (when using old speeches)
+    grep "$speechname" ${meta} | tr "," "\t" > ${datadir}/spkname_speechname.tmp
+    spkID=$(cut -f1 ${datadir}/spkname_speechname.tmp | perl -pe 's/[ \.]//g')
+
+    # Extract the speaker info (to use with new speeches). What will be the format of the new meta files? F.ex. comma separated with speakers name in 1st col?
+    #spkID=$(cut -f1 ${meta} | perl -pe 's/[ \.]//g')
     
-    echo "a) spk2gender" # I use the abbreviation of parliament members' names as speaker IDs
-    cut -f2- $name_id_file > ${datadir}/spk2gender
+    echo "a) utt2spk" # Connect each speech ID to a speaker ID.
+    printf "%s %s\n" ${spkID}-${speechname} ${spkID} | tr -d $'\r' > ${datadir}/utt2spk
 
-    echo "b) utt2spk" # Connect each utterance to a speaker.
-    echo "c) wav.scp" # Connect every utterance with an audio file
-    cut -d"," -f1,6 ${meta} | tr "," "\t" | LC_ALL=C sort > ${datadir}/spkname_filename.tmp
+    # Make a helper file with mapping between the speechnames and uttID
+    echo -e ${speechname} ${spkID}-${speechname} | tr -d $'\r' | LC_ALL=C sort -n > ${datadir}/speechname_uttID.tmp
+    
+    echo "b) wav.scp" # Connect every speech ID with an audio file location.
+    echo -e ${spkID}-${speechname} $wav_cmd" < "$(readlink -f recognize/${speechname}".$ext")" |" | tr -d $'\r' > ${datadir}/wav.scp
+    #echo -e ${spkID}-${speechname} $wav_cmd" < "$(readlink -f data/local/corpus/audio/${speechname}".flac")" |" | tr -d $'\r' > ${datadir}/wav.scp
+    rm ${datadir}/spkname_speechname.tmp ${datadir}/speechname_uttID.tmp
 
-    rm ${datadir}/utt2spk ${datadir}/filename_uttID.txt ${datadir}/wav.scp
-    IFS=$'\n' # Want to separate on new lines
-    for line in $(cat ${datadir}/spkname_filename.tmp)
-    do
-	filename=$(echo $line | cut -f2)
-	spkname=$(echo $line | cut -f1)
-	spkID=$(grep $spkname ${name_id_file} | cut -f2)
-
-	# Print to utt2spk
-	printf "%s %s\n" ${spkID}-${filename} ${spkID} | tr -d $'\r' >> ${datadir}/utt2spk
-
-	# Make a helper file with mapping between the filenames and uttID
-	echo -e ${filename} ${spkID}-${filename} | tr -d $'\r' | LC_ALL=C sort -n >> ${datadir}/filename_uttID.txt
-	
-	#Print to wav.scp
-	#echo -e ${spkID}-${filename} $wav_cmd" < "$(readlink -f ${datadir}/${filename}".mp3")" |" | tr -d $'\r' >> ${datadir}/wav.scp
-	echo -e ${spkID}-${filename} $wav_cmd" < "$(readlink -f ${datadir}/${filename}".flac")" |" | tr -d $'\r' >> ${datadir}/wav.scp
-    done
-    rm ${datadir}/spkname_filename.tmp
-
-    echo "d) spk2utt"
+    echo "c) spk2utt"
     utils/utt2spk_to_spk2utt.pl < ${datadir}/utt2spk > ${datadir}/spk2utt
 fi
 
 if [ $stage -le 1 ]; then
     echo "Extracting features"
     steps/make_mfcc.sh \
-	--nj $num_jobs      \
+	--nj 1 \
 	--mfcc-config conf/mfcc.conf \
 	--cmd "$train_cmd"           \
 	${datadir} || exit 1;

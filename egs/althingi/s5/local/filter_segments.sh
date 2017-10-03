4708c7be4abc727c4c25d06df2ba7b6560846509
Inga Run <ingarunh@gmail.com>
2017-10-03 08:56:12 +0000

    filter based on hard cuts

--- egs/althingi/s5/local/filter_segments.sh
+++ egs/althingi/s5/local/filter_segments.sh
@@ -23,27 +23,33 @@ for s in spk2utt text utt2spk wav.scp wps_stats.txt wps.txt; do
   [ ! -e ${newdir}/$s ] && cp -r ${dir}/$s ${newdir}/$s
 done
 
-LC_ALL=C sort -k2,2 ${newdir}/wps.txt > temp && mv temp ${newdir}/wps.txt
-LC_ALL=C sort -k1,1 ${newdir}/wps_stats.txt > temp && mv temp ${newdir}/wps_stats.txt
-
-wps10percentile=$(cut -d" " -f1 ${newdir}/wps.txt | sort -n | awk '{all[NR] = $0} END{print all[int(NR*0.10 + 0.5)]}')
-wps90percentile=$(cut -d" " -f1 ${newdir}/wps.txt | sort -n | awk '{all[NR] = $0} END{print all[int(NR*0.90 - 0.5)]}')
-
-# Find the ID of the segments that fulfill the requirements.
-IFS=$'\n'
-for line in $(cat ${newdir}/wps_stats.txt); do
-  ID=$(echo $line | cut -d" " -f1)
-  p5=$(echo $line | cut -d" " -f10)
-  p95=$(echo $line | cut -d" " -f14)
-  awk -v var1="$ID" -v var2="$p5" -v var3="$p95" -v var4="$wps10percentile" -v var5="$wps90percentile" '{if ($3 ~ var1 && ($1 > var2 || $1 > var4) && ($1 < var3 || $1 < var5)) print $2}'  \
-      < ${newdir}/wps.txt >> ${newdir}/segm_keep.tmp
-done
+# Filter using constant values
+awk -F" " '{if ($1 > 0.4 && $1 < 5.5) print;}' < wps.txt > ${newdir}/wps_filtered.txt
 
 # Filter the segments
-join -1 1 -2 1 <(sort ${newdir}/segm_keep.tmp) <(sort ${dir}/segments) | LC_ALL=C sort > ${newdir}/segments
+join -1 1 -2 1 <(cut -d" " -f2 ${newdir}/wps_filtered.txt | sort) <(sort ${dir}/segments) | LC_ALL=C sort > ${newdir}/segments
+
+# LC_ALL=C sort -k2,2 ${newdir}/wps.txt > temp && mv temp ${newdir}/wps.txt
+# LC_ALL=C sort -k1,1 ${newdir}/wps_stats.txt > temp && mv temp ${newdir}/wps_stats.txt
+
+# wps1percentile=$(cut -d" " -f1 ${newdir}/wps.txt | sort -n | awk '{all[NR] = $0} END{print all[int(NR*0.01 + 0.5)]}')
+# wps99percentile=$(cut -d" " -f1 ${newdir}/wps.txt | sort -n | awk '{all[NR] = $0} END{print all[int(NR*0.99 - 0.5)]}')
+
+# # Find the ID of the segments that fulfill the requirements.
+# IFS=$'\n'
+# for line in $(cat ${newdir}/wps_stats.txt); do
+#   ID=$(echo $line | cut -d" " -f1)
+#   p1=$(echo $line | cut -d" " -f9)
+#   p99=$(echo $line | cut -d" " -f15)
+#   awk -v var1="$ID" -v var2="$p1" -v var3="$p99" -v var4="$wps1percentile" -v var5="$wps99percentile" '{if ($3 ~ var1 && ($1 > var2 || $1 > var4) && ($1 < var3 || $1 < var5)) print $2}'  \
+#       < ${newdir}/wps.txt >> ${newdir}/segm_keep.tmp
+# done
+
+# # Filter the segments
+# join -1 1 -2 1 <(sort ${newdir}/segm_keep.tmp) <(sort ${dir}/segments) | LC_ALL=C sort > ${newdir}/segments
 
 # Sort and filter the other files
 utils/validate_data_dir.sh --no-feats ${newdir} || utils/fix_data_dir.sh ${newdir}
 
-rm *.tmp
+# rm ${newdir}/*.tmp
 exit 0

be975c209829f862b2a771c923f562b5acc35243
Inga Run <ingarunh@gmail.com>
2017-10-23 12:04:11 +0000

    not using data/lang, only LM dirs

--- egs/althingi/s5/recognize/local/recognize.sh
+++ egs/althingi/s5/recognize/local/recognize.sh
@@ -43,7 +43,6 @@ fi
 
 # Dirs used #
 # Already existing
-langdir=data/lang
 graphdir=exp/chain/tdnn_lstm_1e_sp/graph_3gsmall
 oldLMdir=data/lang_3gsmall
 newLMdir=data/lang_5g
@@ -116,7 +115,78 @@ if [ $stage -le 7 ]; then
     echo "Extract the transcript hypothesis from the Kaldi lattice"
     lattice-best-path \
         --lm-scale=$lmwt \
-        --word-symbol-table=${langdir}/words.txt \
+        --word-symbol-table=${=data/lang_3gsmall
+newLMdir=data/lang_5g
+# Created
+decodedir=${datadir}_segm_hires/decode_3gsmall
+rescoredir=${datadir}_segm_hires/decode_5g
+
+if [ $stage -le 0 ]; then
+
+    echo "Set up a directory in the right format of Kaldi and extract features"
+    recognize/local/prep_audiodata.sh $speechfile $speakerfile $datadir
+fi
+spkID=$(cut -d" " -f1 $datadir/spk2utt)
+
+if [ $stage -le 3 ]; then
+
+    echo "Segment audio data"
+    recognize/local/segment_audio.sh ${datadir} ${datadir}_segm
+fi
+
+if [ $stage -le 4 ]; then
+
+    echo "Create high resolution MFCC features"
+    utils/copy_data_dir.sh ${datadir}_segm ${datadir}_segm_hires
+    steps/make_mfcc.sh \
+	--nj $num_jobs --mfcc-config conf/mfcc_hires.conf \
+        --cmd "$train_cmd" ${datadir}_segm_hires || exit 1;
+    steps/compute_cmvn_stats.sh ${datadir}_segm_hires || exit 1;
+    utils/fix_data_dir.sh ${datadir}_segm_hires
+fi
+
+if [ $stage -le 5 ]; then
+
+    echo "Extracting iVectors"
+    mkdir -p ${datadir}_segm_hires/ivectors_hires
+    steps/online/nnet2/extract_ivectors_online.sh \
+	--cmd "$train_cmd" --nj $num_jobs \
+        ${datadir}_segm_hires exp/chain/extractor \
+        ${datadir}_segm_hires/ivectors_hires || exit 1;
+fi
+
+if [ $stage -le 6 ]; then
+    rm ${datadir}_segm_hires/.error 2>/dev/null || true
+
+    frames_per_chunk=140,100,160
+    frames_per_chunk_primary=$(echo $frames_per_chunk | cut -d, -f1)
+    extra_left_context=50
+    extra_right_context=0
+    cp exp/chain/tdnn_lstm_1e_sp/{final.mdl,final.ie.id,cmvn_opts,frame_subsampling_factor} ${datadir}_segm_hires
+
+    steps/nnet3/decode.sh \
+	--acwt 1.0 --post-decode-acwt 10.0 \
+	--nj $num_jobs --cmd "$decode_cmd" \
+	--skip-scoring true \
+	--extra-left-context $extra_left_context  \
+	--extra-right-context $extra_right_context  \
+	--extra-left-context-initial 0 \
+	--extra-right-context-final 0 \
+	--frames-per-chunk "$frames_per_chunk_primary" \
+	--online-ivector-dir ${datadir}_segm_hires/ivectors_hires \
+	$graphdir ${datadir}_segm_hires ${decodedir} || exit 1;
+    
+    steps/lmrescore_const_arpa.sh --cmd "$decode_cmd" --skip-scoring true \
+	${oldLMdir} ${newLMdir} ${datadir}_segm_hires \
+	${decodedir} ${rescoredir} || exit 1;
+fi
+
+if [ $stage -le 7 ]; then
+
+    echo "Extract the transcript hypothesis from the Kaldi lattice"
+    lattice-best-path \
+        --lm-scale=$lmwt \
+        --word-symbol-table=${oldLMdir}/words.txt \
         "ark:zcat ${rescoredir}/lat.1.gz |" ark,t:- &> ${rescoredir}/extract_transcript.log
 
     # Extract the best path text (tac - concatenate and print files in reverse)
@@ -137,7 +207,7 @@ if [ $score = true ] ; then
 
     echo "Estimate the WER"
     # NOTE! Correct for the mismatch in the beginning and end of recordings.
-    recognize/local/score_recognize.sh --cmd "$train_cmd" $speechname ${langdir} ${rescoredir}
+    recognize/local/score_recognize.sh --cmd "$train_cmd" $speechname ${oldLMdir} ${rescoredir}
 fi
 
 rm -r ${datadir} ${datadir}_segm

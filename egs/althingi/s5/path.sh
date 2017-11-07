a6c80f645cbcaa067a8a4508974f7f901172251e
Judy Fong <jyfong@ucdavis.edu>
2017-11-07 11:57:30 +0000

    to work locally

--- egs/althingi/s5/path.sh
+++ egs/althingi/s5/path.sh
@@ -1,8 +1,8 @@
 export KALDI_ROOT=`pwd`/../../..
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
-export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:/opt/mitlm/bin:/opt/sequitur/bin:$PWD:$PATH
+export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD/tools/mitlm/bin:$PWD/tools/sequitur/bin:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
 
-PYTHONPATH=$PYTHONPATH:/opt/sequitur/lib/python2.7/site-packages/
+PYTHONPATH=$PYTHONPATH:$PWD/tools/sequitur/lib/python2.7/site-packages/
 export PYTHONPATH

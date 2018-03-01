c97d1d355d9fa4784b7b9c146e64d5410ce32f34
Inga Run <ingarunh@gmail.com>
2018-03-01 09:53:38 +0000

    save the urls as well

--- egs/althingi/s5/local/data_extraction/get_mp3_from_timestamps.py
+++ egs/althingi/s5/local/data_extraction/get_mp3_from_timestamps.py
@@ -5,27 +5,36 @@ import codecs
 import sys
 import re
 import urllib
+import os
 
-def getMP3(stamps):
+def getMP3(stamps,furls):
     """Download an mp3 file from the Althingi website given 
     the date of the speech, plus the start and end times"""
 
     for line in stamps:
-        date,start,_,end = line.split()
+        _,date,start,_,end = line.split()
         mp3url = 'http://www.althingi.is/raedur/?start=' + date + 'T' + start + '&end=' + date + 'T' + end
-        print 'mp3url: ' + mp3url
+        furls.write(mp3url + '\n') 
 
         date = re.sub('-', '', date)
         start = re.sub(':', '', start)
         end = re.sub(':', '', end)
 
+        # The uttID
         rad = 'rad' + date + 'T' + start
-        audio_file_name = '/home/staff/inga/kaldi/egs/althingi/s5/recognize/notendaprof2/audio/' + rad + '.mp3'
+
+        # Put the audio files in the same dir as the urls file
+        cwd = os.getcwd()
+        path=os.path.dirname(furls.name)
+        audio_file_name = cwd + "/" + path + "/" + rad + '.mp3'
+
+        # Extract the audio
         urllib.urlretrieve(mp3url, audio_file_name)
 
 
 if __name__ == "__main__":
-    with codecs.open(sys.argv[1],'r',encoding='utf-8') as fstamps:
-        stamps = fstamps.read().splitlines()
+    with codecs.open(sys.argv[2],'a',encoding='utf-8') as furls:
+        with codecs.open(sys.argv[1],'r',encoding='utf-8') as fstamps:
+            stamps = fstamps.read().splitlines()
 
-        getMP3(stamps)
+            getMP3(stamps,furls)

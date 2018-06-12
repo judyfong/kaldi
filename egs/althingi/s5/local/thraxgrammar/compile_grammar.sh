1d5719c0a360c97001cf4766cd0acfef468c5ec9
Inga Run <ingarunh@gmail.com>
2018-06-12 14:51:04 +0000

    moved all grammar files to local/thraxgrammar

--- egs/althingi/s5/local/thraxgrammar/compile_grammar.sh
+++ egs/althingi/s5/local/thraxgrammar/compile_grammar.sh
@@ -3,25 +3,22 @@
 . ./path.sh
 
 # Run from s5 directory
+indir=local/thraxgrammar
+outdir=~/models/text_norm
 
 # Compile the grammar to expand numbers and abbreviations
 # also the one used when selecting which utterances
 # to keep for training an expansion language model
-thraxmakedep local/expand.grm
+thraxmakedep $indir/expand.grm
 make
-
-# Extract the fst from the compiled grammar
-farextract --filename_suffix=".fst" local/expand.far
-mv EXPAND_UTT.fst text_norm
-mv ABBREVIATE_forExpansion.fst text_norm
 
 # Compile the grammar to abbreviate numbers and abbreviations
 # after transcribing text, also the one used to insert periods
 # into abbreviations
-thraxmakedep local/abbreviate.grm
+thraxmakedep $indir/abbreviate.grm
 make
 
 # Extract the fst from the compiled grammar
-farextract --filename_suffix=".fst" local/abbreviate.far
-mv ABBREVIATE.fst text_norm
-mv INS_PERIODS.fst text_norm
+# and move the fst to the output dir
+farextract --filename_suffix=".fst" $indir/expand.far $indir/abbreviate.far
+mv -t $outdir *.fst

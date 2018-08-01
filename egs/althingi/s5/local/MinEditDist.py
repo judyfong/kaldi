0aa42f7073394066b13f0118d18398264f01bd91
Inga Run <ingarunh@gmail.com>
2018-08-01 14:12:25 +0000

    same as previous

--- egs/althingi/s5/local/MinEditDist.py
+++ egs/althingi/s5/local/MinEditDist.py
@@ -8,14 +8,13 @@ import sys
 import re
 from pyxdameraulevenshtein import damerau_levenshtein_distance
 
-def CorrectSpelling(speech,vocab_bb,vocab_endanlegt):
+def CorrectSpelling(speech,vocab_init,vocab_endanlegt):
     """Use Damerau Levenshtein distance to correct the spelling
     in the intermediate texts"""
 
-    for word in vocab_bb:
-        word_dict={}
+    for word in vocab_init:
+        #word_dict={}
         replaced = 0
-
         for w_endanlegt in vocab_endanlegt:
             #dist=MinEditDist(word,w_endanlegt)
             dist = damerau_levenshtein_distance(word, w_endanlegt)
@@ -23,15 +22,14 @@ def CorrectSpelling(speech,vocab_bb,vocab_endanlegt):
                 speech = re.sub(r"\b%s\b" % word,w_endanlegt,speech)
                 replaced = 1
                 break
-            else:
-                word_dict[dist]=w_endanlegt
+        #     else:
+        #         word_dict[dist]=w_endanlegt
                 
-        # Need to find the min dist and substitute if not already substituted
-        if replaced == 0:
-            speech = re.sub(r"\b%s\b" % word,word_dict[min(word_dict,key=int)],speech)
+        # # Need to find the min dist and substitute if not already substituted
+        # if replaced == 0:
+        #     speech = re.sub(r"\b%s\b" % word,word_dict[min(word_dict,key=int)],speech)
             
     return speech
-
 
 if __name__ == "__main__":
 
@@ -39,13 +37,9 @@ if __name__ == "__main__":
     with open(sys.argv[2],'a',encoding='utf-8') as fout:
         with open(sys.argv[1],'r',encoding='utf-8') as fspeech:
             speech = fspeech.read().strip()
-            with open(sys.argv[3],'r',encoding='utf-8') as fvoc_bb:
-                vocab_bb = fvoc_bb.read().splitlines()
+            with open(sys.argv[3],'r',encoding='utf-8') as fvoc_init:
+                vocab_init = fvoc_init.read().splitlines()
                 with open(sys.argv[4],'r',encoding='utf-8') as fvoc_end:
                     vocab_endanlegt  = fvoc_end.read().splitlines()
-
-                    speech_fixed = CorrectSpelling(speech,vocab_bb,vocab_endanlegt)
+                    speech_fixed = CorrectSpelling(speech,vocab_init,vocab_endanlegt)
                     print(speech_fixed, file=fout)
-
-    
-

447d546f2e01401a1dd04b8b8b93ec31de5a1823
Inga Run <ingarunh@gmail.com>
2019-04-24 13:35:32 +0000

    Fix denormalization of sentences with an abbreviation as the first word

--- egs/althingi/s5/punctuator/convert_to_readable.py
+++ egs/althingi/s5/punctuator/convert_to_readable.py
@@ -25,7 +25,7 @@ if __name__ == "__main__":
             else:
                 if token.isupper():
                     out_f.write(('' if first else ' ') + token)
-                else
+                else:
                     out_f.write(('' if first else ' ') + (token.title() if last_was_eos else token))
 
             last_was_eos = token in EOS_TOKENS
